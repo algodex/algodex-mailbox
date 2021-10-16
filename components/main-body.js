a0296de1a14ca8c675a6b3a2d2c869b0f8ee792e
Alexander Trefonas <atrefonas@gmail.com>
2021-10-16 22:53:37 +0530

    Show addresses after connecting wallet

--- components/main-body.js
+++ components/main-body.js
@@ -1,14 +1,38 @@
+import { useMemo, useState } from 'react'
+import useMyAlgo from  '../hooks/use-my-algo'
+
 import {
   MainBodyContainer
 } from './layout.css'
 
 const MainBody = ({children}) => {
+    const [formattedAddresses, setFormattedAddresses] = useState('');
+
+    const updateAddresses = (addresses) => {
+          console.log('in updateAddresses');
+
+      if (addresses == null) {
+        return;
+      }
+      const formatted = addresses.reduce( (sum, address) => {return sum + <br> </br> + address}, '');
+      console.log(formatted);
+      setFormattedAddresses(formatted);
+    };
+
+    const { connect, addresses } = useMyAlgo(updateAddresses)
+
 
     return (
         <MainBodyContainer>
             {children}
+            <p></p>
+            <button onClick={connect}>Connect Wallet</button>
+            <p>{formattedAddresses}</p>
         </MainBodyContainer>
     )
 }
 
 export default MainBody;
+
+
+
