d86a66fb9bb1d8ae1bfce9b4cfb1c7e967ab07fe
Alexander Trefonas <72869083+atrefonas@users.noreply.github.com>
2022-02-26 14:14:41 -0500

    Add transactions to fund and close escrow

--- test/teal.spec.js
+++ test/teal.spec.js
@@ -1,5 +1,6 @@
 const generateTxns = require('../lib/generate_transaction_types.js');
 const testHelper = require('@algodex/algodex-sdk/test_helper.js');
+const algosdk = require('algosdk');
 
 const JEST_MINUTE_TIMEOUT = 60 * 1000;
 const config = {
@@ -23,15 +24,37 @@ describe('Test Mailbox Funding And Withdrawal', () => {
     await testHelper.transferASA(client, openAccount, senderAccount, 1000000, assetId);
   }, JEST_MINUTE_TIMEOUT);
 
-  test('Close Accounts', async () => {
+  test('Fund escrow', async () => {
     const {senderAccount, receiverAccount, openAccount, maliciousAccount, client, assetId} = config;
+    console.log('destructuring');
+    console.log(client);
+
+    const txns = await generateTxns.getFundEscrowTxns(client, assetId, 100000, senderAccount, receiverAccount.addr);
+    const firstTxn = txns[0].unsignedTxn;
+    console.log({firstTxn});
+    const signedTxns = await testHelper.groupAndSignTransactions(txns);
+
+    console.log('printing txns');
+    console.log({signedTxns});
+    await testHelper.sendAndCheckConfirmed(client, signedTxns);
+  }, JEST_MINUTE_TIMEOUT);
+
+  test('Withdraw from escrow', async () => {
+    const {senderAccount, receiverAccount, openAccount, maliciousAccount, client, assetId} = config;
+
+    const txns = await generateTxns.getCloseEscrowTxns(client, assetId, receiverAccount.addr);
+    const firstTxn = txns[0].unsignedTxn;
+    const signedTxns = await testHelper.groupAndSignTransactions(txns);
+
+    await testHelper.sendAndCheckConfirmed(client, signedTxns);
+  }, JEST_MINUTE_TIMEOUT);
+
+  test('Close Accounts', async () => {
+    const {lsigAccount, senderAccount, receiverAccount, openAccount, maliciousAccount, client, assetId} = config;
 
     await testHelper.closeAccount(client, receiverAccount, openAccount);
     await testHelper.closeAccount(client, maliciousAccount, openAccount);
     await testHelper.closeAccount(client, senderAccount, openAccount);
-    
-    const optedIn = await generateTxns.getFundEscrowTxns(null, null, null, null);
-    expect(optedIn).toBe(true);
 
   }, JEST_MINUTE_TIMEOUT);
 
