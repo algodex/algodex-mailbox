e48b7b2cb735c59d397618f201355f5bdbf50a47
Treasurechic <babychic140@gmail.com>
2022-02-28 14:47:08 +0100

    :rotating_light: Send asset, send history, redeem asset tabs created

--- pages/_document.js
+++ pages/_document.js
@@ -33,7 +33,7 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
           <link
             href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;0,700;0,900;1,400;1,500;1,700&display=swap"
             rel="stylesheet"
