2f2caa6e8b4d6b2c92f497acae21ea67bda9a242
Treasurechic <babychic140@gmail.com>
2022-02-28 14:48:53 +0100

    :rotating_light: Fix build error

--- components/header.js
+++ components/header.js
@@ -1,31 +1,34 @@
-import {
-  HeaderC,
-  IconLogo
-} from './layout.css'
-import Link from 'next/link'
+import { HeaderC, IconLogo } from "./layout.css";
+import Link from "next/link";
 
 const Header = () => {
-    const environment = process.env.NEXT_PUBLIC_ALGODEX_ENVIRONMENT || 'public_test';
-    const environmentText = environment == 'production' ? 'Mainnet' : 
-      'Testnet';
+  const environment =
+    process.env.NEXT_PUBLIC_ALGODEX_ENVIRONMENT || "public_test";
+  const environmentText = environment == "production" ? "Mainnet" : "Testnet";
 
-    return (
-        <HeaderC>
-          <div>
-          <Link href='/'>
-            <IconLogo src="/algodex-logo.svg" style={{cursor:'pointer'}}/>
-          </Link>
-          <div style={{whiteSpace: 'pre-wrap',
-            marginLeft: 5, height: 20, paddingTop: 5,
-            display: 'flex',
-            'fontStyle': 'italic', 'fontSize': '14px', color:'blue'}}>
-            <span>
-            {environmentText}
-            </span></div>
-            </div>
-        </HeaderC> 
+  return (
+    <HeaderC>
+      <div>
+        <Link href="/">
+          <IconLogo src="/algodex-logo.svg" style={{ cursor: "pointer" }} />
+        </Link>
+        <div
+          style={{
+            whiteSpace: "pre-wrap",
+            marginLeft: 5,
+            height: 20,
+            paddingTop: 5,
+            display: "flex",
+            fontStyle: "italic",
+            fontSize: "14px",
+            color: "blue",
+          }}
+        >
+          <span>{environmentText}</span>
+        </div>
+      </div>
+    </HeaderC>
+  );
+};
 
-    )
-}
-
-export default Header
+export default Header;
