d790bc06808d5c621c3be94563ae4afec8db66cf
Treasurechic <babychic140@gmail.com>
2022-03-04 17:40:00 +0100

    :zap: Display error and success message after sending assets

--- components/RedeemAssetForm.js
+++ components/RedeemAssetForm.js
@@ -1,32 +1,32 @@
-import React from "react";
-import { MuiForm5 as Form } from "@rjsf/material-ui";
-import Grid from "@mui/material/Grid";
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
-import PropTypes from "prop-types";
+import React from 'react'
+import { MuiForm5 as Form } from '@rjsf/material-ui'
+import Grid from '@mui/material/Grid'
+import Button from '@mui/material/Button'
+import Typography from '@mui/material/Typography'
+import PropTypes from 'prop-types'
 
 const RedeemAssetForm = ({ onSubmit, actionStatus }) => {
   const schema = {
-    required: ["assetId", "walletAddress"],
+    required: ['assetId', 'walletAddress'],
     properties: {
-      assetId: { type: "string", title: "Asset Id" },
-      walletAddress: { type: "string", title: "Wallet Address" },
+      assetId: { type: 'string', title: 'Asset Id' },
+      walletAddress: { type: 'string', title: 'Wallet Address' },
     },
-  };
+  }
 
   return (
     <Form schema={schema} onSubmit={onSubmit}>
-      <Grid container spacing={2} marginTop={"2rem"} marginBottom={"2rem"}>
+      <Grid container spacing={2} marginTop={'2rem'} marginBottom={'2rem'}>
         <Grid item xs={6} lg={4}>
           <Button variant="contained" type="submit">
             Redeem
           </Button>
         </Grid>
         <Grid item xs={6}>
-          {actionStatus.message != "" && (
+          {actionStatus.message != '' && (
             <Typography
               variant="error-message"
-              color={actionStatus.success ? "green" : "error"}
+              color={actionStatus.success ? 'green' : 'error'}
             >
               {actionStatus.message}
             </Typography>
@@ -34,10 +34,10 @@ const RedeemAssetForm = ({ onSubmit, actionStatus }) => {
         </Grid>
       </Grid>
     </Form>
-  );
-};
+  )
+}
 
 RedeemAssetForm.propTypes = {
   onSubmit: PropTypes.func.isRequired,
-};
-export default RedeemAssetForm;
+}
+export default RedeemAssetForm
--- pages/send-asset.js
+++ pages/send-asset.js
@@ -1,20 +1,20 @@
-import React, { useCallback, useState } from "react";
-import { serverSideTranslations } from "next-i18next/serverSideTranslations";
-import Head from "next/head";
+import React, { useCallback, useState } from 'react'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import Head from 'next/head'
 
 // MUI Components
-import Container from "@mui/material/Container";
-import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
-import Grid from "@mui/material/Grid";
+import Container from '@mui/material/Container'
+import Typography from '@mui/material/Typography'
+import Button from '@mui/material/Button'
+import Grid from '@mui/material/Grid'
 
 // Custom Components
-import * as SendAssetsHelper from "@/lib/send_assets.js";
-import SendAssetForm from "@/components/SendAssetForm";
-import Link from "@/components/Nav/Link";
-import useMyAlgo from "@/hooks/use-my-algo";
-import { defaults } from "next-i18next.config";
-import { useTranslation } from "next-i18next";
+import * as SendAssetsHelper from '@/lib/send_assets.js'
+import SendAssetForm from '@/components/SendAssetForm'
+import Link from '@/components/Nav/Link'
+import useMyAlgo from '@/hooks/use-my-algo'
+import { defaults } from 'next-i18next.config'
+import { useTranslation } from 'next-i18next'
 
 /**
  * Generate Static Properties
@@ -25,7 +25,7 @@ export async function getStaticProps({ locale }) {
     props: {
       ...(await serverSideTranslations(locale, [...defaults])),
     },
-  };
+  }
 }
 
 /**
@@ -34,60 +34,90 @@ export async function getStaticProps({ locale }) {
  * @constructor
  */
 export function SendAssetPage() {
-  const [actionStatus, setActionStatus] = useState("");
-  const { t } = useTranslation("common");
-  const [formattedAddresses, setFormattedAddresses] = useState([]);
+  const [loading, setLoading] = useState(false)
+  const [actionStatus, setActionStatus] = useState({
+    message: '',
+    success: false,
+  })
+  const { t } = useTranslation('common')
+  const [formattedAddresses, setFormattedAddresses] = useState([])
 
   const updateAddresses = useCallback(
     (addresses) => {
       if (addresses == null) {
-        return;
+        return
       }
-      console.log({ addresses });
-      setFormattedAddresses(addresses);
+      console.log({ addresses })
+      setFormattedAddresses(addresses)
     },
     [setFormattedAddresses]
-  );
+  )
 
-  const { connect } = useMyAlgo(updateAddresses);
+  const { connect } = useMyAlgo(updateAddresses)
   const submitForm = async ({ formData }) => {
-    const { wallet, assetId, csvTransactions } = formData;
-    console.log({ formData });
+    const { wallet, assetId, csvTransactions } = formData
+    console.log({ formData })
+    setLoading(true)
     const responseData = await SendAssetsHelper.send(
       assetId,
       wallet,
       csvTransactions
-    );
-    console.log("responseData", responseData);
-    setActionStatus(responseData.body.message);
-  };
+    )
+    console.log('responseData', responseData)
+    setLoading(false)
+    if (responseData.error == false) {
+      const totalAssets = responseData.confirmedTransactions.length
+      const sentAssets = responseData.confirmedTransactions.filter(
+        (asset) => asset.value.status == 'confirmed'
+      ).length
+      setActionStatus({
+        message: `${sentAssets}/${totalAssets} asset(s) sent successfully`,
+        success: true,
+      })
+    } else {
+      setActionStatus({
+        message: responseData.body?.message || 'Sorry an error occured',
+        success: false,
+      })
+    }
+  }
   return (
     <>
       <Head>
-        <title>{`${t("/send-asset")} | ${t("app-title")}`}</title>
+        <title>{`${t('/send-asset')} | ${t('app-title')}`}</title>
       </Head>
       <Container sx={{ my: 4 }}>
         <Grid container spacing={2}>
           <Grid item xs={12} md={8} lg={6} xl={5}>
-            <Typography variant="h5" sx={{ marginBottom: "1rem" }}>
-              {t("/send-asset")}
+            <Typography variant="h5" sx={{ marginBottom: '1rem' }}>
+              {t('/send-asset')}
             </Typography>
             <Button variant="contained" onClick={connect}>
-              {t("connect-wallet")}
+              {t('connect-wallet')}
             </Button>
             <SendAssetForm
               formattedAddresses={formattedAddresses}
               onSubmit={submitForm}
+              isLoading={loading}
             />
-            <Grid container spacing={2} sx={{ marginTop: "2rem" }}>
+            {actionStatus.message != '' && (
+              <Typography
+                variant="error-message"
+                sx={{ display: 'flex', justifyContent: 'end' }}
+                color={actionStatus.success ? 'green' : 'error'}
+              >
+                {actionStatus.message}
+              </Typography>
+            )}
+            <Grid container spacing={2} sx={{ marginTop: '2rem' }}>
               <Grid item xs={6} lg={5} className="mr-2">
-                <Link href={"/instructions"}>
-                  {t("view-instructions-link")}
+                <Link href={'/instructions'}>
+                  {t('view-instructions-link')}
                 </Link>
               </Grid>
               <Grid item xs={6} lg={5}>
-                <Link href={"/downloadlink"}>
-                  {t("download-csv-example-link")}
+                <Link href={'/downloadlink'}>
+                  {t('download-csv-example-link')}
                 </Link>
               </Grid>
             </Grid>
@@ -95,7 +125,7 @@ export function SendAssetPage() {
         </Grid>
       </Container>
     </>
-  );
+  )
 }
 
-export default SendAssetPage;
+export default SendAssetPage
