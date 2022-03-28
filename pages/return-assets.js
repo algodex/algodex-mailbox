51d054cbf42ef339c64eed4e6501e5caaf260e98
Treasurechic <babychic140@gmail.com>
2022-03-28 20:49:39 +0100

    :bug: Appropriate error message when balance is 0

--- pages/return-assets.js
+++ pages/return-assets.js
@@ -73,7 +73,7 @@ export function ReturnAssetPage() {
         senderAddress,
         csvTransactions
       )
-      // console.debug('responseData', responseData)
+      console.debug('responseData', responseData)
       setLoading(false)
       if (responseData?.error == false) {
         const totalAssets = responseData.confirmedTransactions.length
