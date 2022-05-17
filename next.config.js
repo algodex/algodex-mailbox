0dd367de205ea7f189a497beea1fa0042485f8cd
Treasurechic <babychic140@gmail.com>
2022-05-17 18:39:46 +0100

    :sparkles: Search asset id

--- next.config.js
+++ next.config.js
@@ -1,30 +1,20 @@
 const withPWA = require('next-pwa')
 const { i18n } = require('./next-i18next.config')
 /** @type {import('next').NextConfig} */
-
-const moduleExports = withPWA({
+const nextConfig = {
   // compiler: {
   //   // ssr and displayName are configured by default
   //   styledComponents: true,
+  // },
+  reactStrictMode: true,
   i18n,
   pwa: {
     dest: 'public',
-    disable: process.env.NODE_ENV === 'development',
+    disable: process.env.NODE_ENV === 'development'
   },
   compiler: {
     styledComponents: true,
   },
+}
 
-  async rewrites() {
-    return {
-      beforeFiles: [
-        {
-          source: '/algod/:path*',
-          destination: 'https://indexer.algoexplorerapi.io/rl/v1/search/:path*',
-        },
-      ],
-    }
-  },
-})
-
-module.exports = moduleExports
+module.exports = withPWA(nextConfig)
